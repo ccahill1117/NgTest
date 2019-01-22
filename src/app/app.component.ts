35391e4c7e9d2dbeadf6b10d69d184323a68715e
Christopher Cahill and Ngan Nguyen <ngan@>
2019-01-22 11:49:02 -0800

    trying to nest obj properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model'
+import { Recipe } from './models/recipe.model'
 
 @Component({
-  selector: 'app-root',
+  selector: 'super-component',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Ngan and Chris make an angular thing';
+  currentTime = new Date();
+  currentFocus: string = 'Angular Homework';
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year:number = this.currentTime.getFullYear();
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course.');
+    new Task('Begin brainstorming possible JS group projects');
+    new Task('Add README file to last few Angular repos on GitHub');
+  ];
+  currentRecipes: string = 'Here are our recipes';
+  recipes : Recipe[] = [
+    new Recipe('brownies',['sugar','chocolate','milk'],['mix','bake','cut']);
+    new Recipe('pizza',['flour','cheese','pepperoni'],['mix','bake','cut piza into slices']);
+
+  ]
+
 }
